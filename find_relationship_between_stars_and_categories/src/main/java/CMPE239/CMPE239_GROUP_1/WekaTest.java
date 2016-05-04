10831d1327b58e2dcd228d591130e32fa2752161
xiaofengjohn <xiaofenglijohn@gmail.com>
2016-05-03 20:17:17 -0700

    Add weka comments.

--- find_relationship_between_stars_and_categories/src/main/java/CMPE239/CMPE239_GROUP_1/WekaTest.java
+++ find_relationship_between_stars_and_categories/src/main/java/CMPE239/CMPE239_GROUP_1/WekaTest.java
@@ -7,32 +7,37 @@ import weka.core.DistanceFunction;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 
+/**
+ * Invoke the api of Weka and produce the result which we want.
+ * @author xiaofengli
+ *
+ */
 public class WekaTest {
 
 	public static void main(String[] args) {
-Instances ins = null;
 		
+		Instances ins = null;
+
 		SimpleKMeans KM = null;
 
 		DistanceFunction disFun = null;
-		
+
 		try {
-			File file = new File("/Users/xiaofengli/Downloads/yelp_dataset_challenge_academic_dataset/test.arff");
+			File file = new File("test.arff");
 			ArffLoader loader = new ArffLoader();
 			loader.setFile(file);
 			ins = loader.getDataSet();
-			
+
 			KM = new SimpleKMeans();
-			KM.setNumClusters(20); 
-			KM.buildClusterer(ins);	
+			KM.setNumClusters(20);
+			KM.buildClusterer(ins);
 			KM.setMaxIterations(10);
-			
+
 			System.out.println(KM.toString());
-			for(String option : KM.getOptions()) {
+			for (String option : KM.getOptions()) {
 				System.out.println(option);
 			}
-			//System.out.println("CentroIds:" + tempIns);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
